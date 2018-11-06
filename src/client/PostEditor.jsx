d08494c861c57f103f73a3b26e05645207fe2110
DanielFGray <DanielFGray@gmail.com>
2018-11-06 05:53:26 -0600

    is this more mobile friendly

--- src/client/PostEditor.jsx
+++ src/client/PostEditor.jsx
@@ -5,9 +5,9 @@ import { markdown } from './utils'
 export const UncontrolledEditor = ({ onChange, value, children, ...props }) => {
   const content = value || children
   return (
-    <div className="editor">
+    <>
       <Textarea onChange={onChange} value={content} {...props} />
       <div className="body md_preview">{markdown(content)}</div>
-    </div>
+    </>
   )
 }
